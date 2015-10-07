81fcf0656650bbc0632ad67970659d1ada4790c6
Jesse Panganiban <me@jpanganiban.com>
2015-10-07 13:24:14 +0800

    DB Connection settings.

--- src/config.py
+++ src/config.py
@@ -18,3 +18,7 @@ REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', None)
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
 AWS_REGION = os.environ.get('AWS_REGION', None)
+
+SQLALCHEMY_POOL_SIZE = int(os.environ.get("SQLALCHEMY_POOL_SIZE", 20))
+SQLALCHEMY_POOL_TIMEOUT = int(os.environ.get("SQLALCHEMY_POOL_TIMEOUT", 30))
+SQLALCHEMY_MAX_OVERFLOW = int(os.environ.get("SQLALCHEMY_MAX_OVERFLOW", 10))
